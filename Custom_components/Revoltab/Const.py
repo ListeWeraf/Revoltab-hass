bf1a70b282d3504110edcdf47bf87a0b10641e43
ListeWeraf <liste.weraf@gmail.com>
2026-02-27 18:51:18 +0100

    Add constants for Revoltab integration
    Define constants for Revoltab integration including API endpoints and configuration attributes.

---
+++ Custom_components/Revoltab/Const.py
@@ -0,0 +1,10 @@
+DOMAIN = "revoltab"
+
+API_BASE = "https://api2.o80y8sax.afero.net/v1"
+AUTH_BASE = "https://auth1.o80y8sax.afero.net"
+
+CONF_EMAIL = "email"
+CONF_PASSWORD = "password"
+
+ATTR_POWER = 1
+ATTR_INTENSITY = 4
