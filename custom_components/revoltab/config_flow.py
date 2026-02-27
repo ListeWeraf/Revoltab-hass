f55fb6ff94c5ee4016953677f57d5b30b056b197
ListeWeraf <liste.weraf@gmail.com>
2026-02-27 19:33:35 +0100

    Add error handling for API connection in config flow
    Handle connection errors during API setup in config flow.

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -1,25 +1,36 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.exceptions import HomeAssistantError
 from .const import DOMAIN, CONF_EMAIL, CONF_PASSWORD
 from .api import RevoltabAPI
 
 class RevoltabConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            api = RevoltabAPI(
-                user_input[CONF_EMAIL],
-                user_input[CONF_PASSWORD]
-            )
-            await api.async_setup()
+        errors = {}
 
-            return self.async_create_entry(
-                title="Revoltab",
-                data=user_input
-            )
+        if user_input is not None:
+            try:
+                api = RevoltabAPI(
+                    user_input[CONF_EMAIL],
+                    user_input[CONF_PASSWORD]
+                )
+                await api.async_setup()
+
+                return self.async_create_entry(
+                    title="Revoltab",
+                    data=user_input
+                )
+
+            except Exception as e:
+                errors["base"] = "cannot_connect"
 
         schema = vol.Schema({
             vol.Required(CONF_EMAIL): str,
             vol.Required(CONF_PASSWORD): str,
         })
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors
+        )
