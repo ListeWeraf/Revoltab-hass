9f21b5baa2c3001f059484a435bccbbc18979bf6
ListeWeraf <liste.weraf@gmail.com>
2026-03-01 17:51:42 +0100

    config_flow.py aktualisieren

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -1,36 +1,44 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.exceptions import HomeAssistantError
+
 from .const import DOMAIN, CONF_EMAIL, CONF_PASSWORD
-from .api import RevoltabAPI
+from .api import RevoltabAuth
+
 
 class RevoltabConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
             try:
-                api = RevoltabAPI(
+                auth = RevoltabAuth(
                     user_input[CONF_EMAIL],
-                    user_input[CONF_PASSWORD]
+                    user_input[CONF_PASSWORD],
                 )
-                await api.async_setup()
+
+                await auth.login()
+                await auth.close()
 
                 return self.async_create_entry(
                     title="Revoltab",
-                    data=user_input
+                    data=user_input,
                 )
 
-            except Exception as e:
+            except Exception:
                 errors["base"] = "cannot_connect"
 
-        schema = vol.Schema({
-            vol.Required(CONF_EMAIL): str,
-            vol.Required(CONF_PASSWORD): str,
-        })
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_EMAIL): str,
+                vol.Required(CONF_PASSWORD): str,
+            }
+        )
 
         return self.async_show_form(
             step_id="user",
             data_schema=schema,
-            errors=errors
+            errors=errors,
         )
