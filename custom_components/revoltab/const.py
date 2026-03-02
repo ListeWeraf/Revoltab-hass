6ae40d576aaed921a5793f400a58a63b7f9fd07a
ListeWeraf <liste.weraf@gmail.com>
2026-03-02 08:39:07 +0100

    const.py erstellen

---
+++ custom_components/revoltab/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "revoltab"
+
+AUTH_BASE = "https://auth1.o80y8sax.afero.net"
+TOKEN_URL = f"{AUTH_BASE}/auth/realms/rvt/protocol/openid-connect/token"
+AUTHORIZE_URL = f"{AUTH_BASE}/auth/realms/rvt/protocol/openid-connect/auth"
+
+CLIENT_ID = "rvt_ios"
+SCOPES = ["openid"]
