182b860b7a1d2151154a40db90afbb0187f946f3
ListeWeraf <liste.weraf@gmail.com>
2026-03-02 08:45:15 +0100

    confi_flow.py erstellen

---
+++ custom_components/revoltab/confi_flow.py
@@ -0,0 +1,42 @@
+from homeassistant import config_entries
+from homeassistant.helpers import config_entry_oauth2_flow
+
+from .const import DOMAIN, OAUTH2_AUTHORIZE, OAUTH2_TOKEN, CLIENT_ID, SCOPES
+
+
+class RevoltabOAuth2Implementation(
+    config_entry_oauth2_flow.LocalOAuth2Implementation
+):
+    """OAuth2 implementation for Revoltab."""
+
+    def __init__(self, hass):
+        super().__init__(
+            hass,
+            DOMAIN,
+            CLIENT_ID,
+            None,
+            OAUTH2_AUTHORIZE,
+            OAUTH2_TOKEN,
+            SCOPES,
+        )
+
+
+class RevoltabConfigFlow(
+    config_entry_oauth2_flow.AbstractOAuth2FlowHandler,
+    domain=DOMAIN,
+):
+    """Config flow for Revoltab."""
+
+    DOMAIN = DOMAIN
+
+    async def async_oauth_create_entry(self, data):
+        """Create entry after OAuth."""
+        return self.async_create_entry(
+            title="Revoltab",
+            data=data,
+        )
+
+    async def async_step_user(self, user_input=None):
+        """Start OAuth flow."""
+        implementation = RevoltabOAuth2Implementation(self.hass)
+        return await self.async_step_oauth2(implementation)
