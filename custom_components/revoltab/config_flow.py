b9b0672ee587eeeccbb4624edcc63b3d841fec19
ListeWeraf <liste.weraf@gmail.com>
2026-03-02 15:53:55 +0100

    config_flow.py aktualisieren

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -7,7 +7,7 @@ from .const import DOMAIN, OAUTH2_AUTHORIZE, OAUTH2_TOKEN, CLIENT_ID, SCOPES
 class RevoltabOAuth2Implementation(
     config_entry_oauth2_flow.LocalOAuth2Implementation
 ):
-    """OAuth2 implementation for Revoltab."""
+    """OAuth2 implementation."""
 
     def __init__(self, hass):
         super().__init__(
@@ -21,22 +21,17 @@ class RevoltabOAuth2Implementation(
         )
 
 
-class RevoltabConfigFlow(
-    config_entry_oauth2_flow.AbstractOAuth2FlowHandler
-):
-    """Config flow for Revoltab."""
+class ConfigFlow(config_entry_oauth2_flow.AbstractOAuth2FlowHandler):
+    """Handle a config flow for Revoltab."""
 
-    DOMAIN = DOMAIN
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+    domain = DOMAIN
 
     async def async_oauth_create_entry(self, data):
-        """Create entry after OAuth."""
         return self.async_create_entry(
             title="Revoltab",
             data=data,
         )
 
     async def async_step_user(self, user_input=None):
-        """Start OAuth flow."""
         implementation = RevoltabOAuth2Implementation(self.hass)
         return await self.async_step_oauth2(implementation)
