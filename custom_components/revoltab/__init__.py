7fbf29ea0db314a175e3f2b4321716156b4ec7e7
ListeWeraf <liste.weraf@gmail.com>
2026-03-02 08:39:40 +0100

    __init__.py erstellen

---
+++ custom_components/revoltab/__init__.py
@@ -0,0 +1,21 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import config_entry_oauth2_flow
+
+from .const import DOMAIN
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Revoltab from config entry."""
+    oauth_session = config_entry_oauth2_flow.OAuth2Session(hass, entry)
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = oauth_session
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
