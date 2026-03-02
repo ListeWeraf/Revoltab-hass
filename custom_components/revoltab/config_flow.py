8d6f997e1287d4c02e3de05bf098942892eb98f3
ListeWeraf <liste.weraf@gmail.com>
2026-03-02 15:58:55 +0100

    config_flow.py aktualisieren

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -1,37 +1,13 @@
 from homeassistant import config_entries
-from homeassistant.helpers import config_entry_oauth2_flow
 
-from .const import DOMAIN, OAUTH2_AUTHORIZE, OAUTH2_TOKEN, CLIENT_ID, SCOPES
-
-
-class RevoltabOAuth2Implementation(
-    config_entry_oauth2_flow.LocalOAuth2Implementation
-):
-    """OAuth2 implementation."""
-
-    def __init__(self, hass):
-        super().__init__(
-            hass,
-            DOMAIN,
-            CLIENT_ID,
-            None,
-            OAUTH2_AUTHORIZE,
-            OAUTH2_TOKEN,
-            SCOPES,
-        )
+from .const import DOMAIN
 
 
-class ConfigFlow(config_entry_oauth2_flow.AbstractOAuth2FlowHandler):
-    """Handle a config flow for Revoltab."""
-
-    domain = DOMAIN
-
-    async def async_oauth_create_entry(self, data):
-        return self.async_create_entry(
-            title="Revoltab",
-            data=data,
-        )
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        implementation = RevoltabOAuth2Implementation(self.hass)
-        return await self.async_step_oauth2(implementation)
+        return self.async_create_entry(
+            title="Revoltab Test",
+            data={},
+        )
