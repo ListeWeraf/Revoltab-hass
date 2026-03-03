985d1c5e360b2607c50772b9450c580094b3f235
ListeWeraf <liste.weraf@gmail.com>
2026-03-03 18:09:05 +0100

    config_flow.py aktualisieren

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -1,13 +1,38 @@
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import DOMAIN
+from .oauth import RevoltabOAuth
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        return self.async_create_entry(
-            title="Revoltab",
-            data={},
+        errors = {}
+
+        if user_input is not None:
+            try:
+                oauth = RevoltabOAuth(self.hass)
+                tokens = await oauth.login(
+                    user_input["email"],
+                    user_input["password"],
+                )
+
+                return self.async_create_entry(
+                    title="Revoltab",
+                    data=tokens,
+                )
+
+            except Exception as e:
+                errors["base"] = "auth_failed"
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("email"): str,
+                vol.Required("password"): str,
+            }),
+            errors=errors,
         )
