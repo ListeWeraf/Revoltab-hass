74e4f8c19ceb5edbcc0ab3d173ccfc60366ea343
ListeWeraf <liste.weraf@gmail.com>
2026-03-03 18:11:13 +0100

    const.py aktualisieren

--- custom_components/revoltab/const.py
+++ custom_components/revoltab/const.py
@@ -1,7 +1,1 @@
 DOMAIN = "revoltab"
-
-OAUTH2_AUTHORIZE = "https://auth1.o80y8sax.afero.net/auth/realms/rvt/protocol/openid-connect/auth"
-OAUTH2_TOKEN = "https://auth1.o80y8sax.afero.net/auth/realms/rvt/protocol/openid-connect/token"
-
-CLIENT_ID = "rvt_ios"
-SCOPES = ["openid"]
