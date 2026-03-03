741df29511ce6d465a0d1de2cd73790fc9710fc4
ListeWeraf <liste.weraf@gmail.com>
2026-03-03 17:26:11 +0100

    __init__.py aktualisieren

--- custom_components/revoltab/__init__.py
+++ custom_components/revoltab/__init__.py
@@ -1,24 +1,12 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_entry_oauth2_flow
 
 from .const import DOMAIN
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up Revoltab."""
-
-    session = config_entry_oauth2_flow.OAuth2Session(
-        hass,
-        entry,
-        config_entry_oauth2_flow.async_get_config_entry_implementation(
-            hass, entry
-        ),
-    )
-
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = session
-
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     return True
 
 
