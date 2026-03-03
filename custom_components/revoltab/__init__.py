ef9b9028805d563ae24e86ca72dee91f535d339f
ListeWeraf <liste.weraf@gmail.com>
2026-03-03 19:21:30 +0100

    __init__.py aktualisieren

--- custom_components/revoltab/__init__.py
+++ custom_components/revoltab/__init__.py
@@ -1,12 +1,28 @@
+from datetime import datetime, timedelta
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .oauth import RevoltabOAuth
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    oauth = RevoltabOAuth(hass)
+
+    token_data = dict(entry.data)
+
+    expires_at = datetime.utcnow() + timedelta(
+        seconds=token_data["expires_in"]
+    )
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "oauth": oauth,
+        "tokens": token_data,
+        "expires_at": expires_at,
+    }
+
     return True
 
 
